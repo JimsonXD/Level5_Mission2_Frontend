ee800aebcfb0eae661c298920006085ef35e9bec
Waterclone <jene@missionreadyhq.com>
2023-08-23 22:48:53 +1200

    Restore from TS mishandling

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function Carousel() {
-    return (
+  return (
     <div>
       <div className="carousel w-full my-2">
         <div id="slide1" className="carousel-item relative w-full">
-          <img src="/images/tur11.jpeg" className="object-cover w-full h-64 md:h-96 lg:h-128" />
+          <img src="/images/tur11.jpeg" alt="" className="object-cover w-full h-64 md:h-96 lg:h-128" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -16,7 +16,7 @@ export default function Carousel() {
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <img src="/images/tur12.jpeg" className="object-cover w-full h-64 md:h-96 lg:h-128" />
+          <img src="/images/tur12.jpeg" alt="" className="object-cover w-full h-64 md:h-96 lg:h-128" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -27,7 +27,7 @@ export default function Carousel() {
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <img src="/images/tur11.jpeg" className="object-cover w-full h-64 md:h-96 lg:h-128" />
+          <img src="/images/tur11.jpeg" alt="" className="object-cover w-full h-64 md:h-96 lg:h-128" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -38,7 +38,7 @@ export default function Carousel() {
           </div>
         </div>
         <div id="slide4" className="carousel-item relative w-full">
-          <img src="/images/tur12.jpeg" className="object-cover w-full h-64 md:h-96 lg:h-128" />
+          <img src="/images/tur12.jpeg" alt="" className="object-cover w-full h-64 md:h-96 lg:h-128" />
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">
               ❮
